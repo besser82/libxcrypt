c5601ca16dc3a6a8d8b708dbc1ed80d621d3f776
Zack Weinberg <zackw@panix.com>
2017-09-12 17:07:44 -0400

    Don't use a system random device in two tests.
    Both of these tests were reading from /dev/urandom to generate salts,
    which is unnecessary and makes the test nondeterministic.  Replace with
    hardcoded random strings.  Also, gensalt-test.c is now somewhat more
    thorough.
    
    	* src/bigcrypt-test.c: Don't use /dev/urandom.
    	* src/gensalt-test.c: Likewise.  Test more thoroughly.

--- src/gensalt-test.c
+++ src/gensalt-test.c
@@ -1,109 +1,81 @@
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
 #include <stdio.h>
-#include <signal.h>
-#include <sys/time.h>
-#include <sys/times.h>
 
 #include "crypt.h"
 
-#ifndef RANDOM_DEVICE
-#define RANDOM_DEVICE "/dev/urandom"
-#endif
+static const char *const entropy[] = {
+  "\x58\x35\xcd\x26\x03\xab\x2c\x14\x92\x13\x1e\x59\xb0\xbc\xfe\xd5",
+  "\x9b\x35\xa2\x45\xeb\x68\x9e\x8f\xd9\xa9\x09\x71\xcc\x4d\x21\x44",
+  "\x25\x13\xc5\x94\xc3\x93\x1d\xf4\xfd\xd4\x4f\xbd\x10\xe5\x28\x08",
+  "\xa0\x2d\x35\x70\xa8\x0b\xc3\xad\xdf\x61\x69\xb3\x19\xda\x7e\x8d",
+  0
+};
 
-static int
-read_loop (int fd, char *buffer, int count)
+struct testcase
 {
-  int offset, block;
+  const char *prefix;
+  unsigned int expected_len;
+};
 
-  offset = 0;
-  while (count > 0)
-    {
-      block = read(fd, &buffer[offset], count);
-
-      if (block < 0)
-        {
-          if (errno == EINTR)
-            continue;
-          return block;
-        }
-      if (!block)
-        return offset;
-
-      offset += block;
-      count -= block;
-    }
-
-  return offset;
-}
-
-static char *
-make_crypt_salt (const char *crypt_prefix, int crypt_rounds)
-{
-#define CRYPT_GENSALT_OUTPUT_SIZE (7 + 22 + 1)
-  int fd;
-  char entropy[16];
-  char *retval;
-  char output[CRYPT_GENSALT_OUTPUT_SIZE];
-
-  fd = open (RANDOM_DEVICE, O_RDONLY);
-  if (fd < 0)
-    {
-      fprintf (stderr, "Can't open %s for reading: %s\n",
-	       RANDOM_DEVICE, strerror (errno));
-      return NULL;
-    }
-
-  if (read_loop (fd, entropy, sizeof(entropy)) != sizeof(entropy))
-    {
-      close (fd);
-      fprintf (stderr, "Unable to obtain entropy from %s\n",
-	       RANDOM_DEVICE);
-      return NULL;
-    }
-
-  close (fd);
-
-  retval = crypt_gensalt_r (crypt_prefix, crypt_rounds, entropy,
-                            sizeof (entropy), output, sizeof(output));
-
-  memset (entropy, 0, sizeof (entropy));
-
-  if (!retval)
-    {
-      fprintf (stderr,
-	       "Unable to generate a salt, check your crypt settings.\n");
-      return NULL;
-    }
-
-  return strdup (retval);
-}
-
-static char *salt_input[] =
-  { "", "$1$", "$2a$" , "$5$", "$6$" };
+const struct testcase testcases[] = {
+  { "", 2 }, // DES
+  { "_", 9 }, // BSDi extended DES
+  { "$1$", 11 }, // MD5
+  { "$5$", 11 }, // SHA-2-256
+  { "$6$", 11 }, // SHA-2-512
+  { "$2a$", 29 }, // bcrypt mode A
+  { "$2b$", 29 }, // bcrypt mode B
+  { "$2x$", 29 }, // bcrypt mode X
+  { "$2y$", 29 }, // bcrypt mode Y
+  { 0, 0 }
+};
 
 int
-main(void)
+main (void)
 {
-  int i;
   int status = 0;
+  unsigned int ent;
+  const struct testcase *tcase;
+  char output[CRYPT_GENSALT_OUTPUT_SIZE];
+  char prev_output[CRYPT_GENSALT_OUTPUT_SIZE];
 
-  for (i = 0; i < (int) (sizeof (salt_input) / sizeof (salt_input[0])); i++)
+  for (tcase = testcases; tcase->prefix; tcase++)
     {
-      char *salt = make_crypt_salt (salt_input[i], 0);
+      memset (prev_output, 0, CRYPT_GENSALT_OUTPUT_SIZE);
+      for (ent = 0; entropy[ent]; ent++)
+        {
+          memset (output, 0, CRYPT_GENSALT_OUTPUT_SIZE);
+          char *salt = crypt_gensalt_r (tcase->prefix, 0,
+                                        entropy[ent], 16,
+                                        output, CRYPT_GENSALT_OUTPUT_SIZE);
+          unsigned int slen = strlen (salt);
+          if (slen != tcase->expected_len)
+            {
+              fprintf (stderr, "ERROR: %s/%u -> %s (expected len=%u got %u)\n",
+                       tcase->prefix, ent, salt,
+                       tcase->expected_len, slen);
+              status = 1;
+            }
+          else if (strncmp (salt, tcase->prefix, strlen (tcase->prefix)))
+            {
+              fprintf (stderr, "ERROR: %s/%u -> %s (prefix wrong)\n",
+                       tcase->prefix, ent, salt);
+              status = 1;
+            }
+          else if (!strcmp (salt, prev_output))
+            {
+              fprintf (stderr, "ERROR: %s/%u -> %s (same as prev)\n",
+                       tcase->prefix, ent, salt);
+              status = 1;
+            }
+          else
+            fprintf (stderr, "   ok: %s/%u -> %s\n",
+                     tcase->prefix, ent, salt);
 
-      int ok = (salt_input[i][0] == '\0' ||
-                strncmp (salt_input[i], salt, strlen (salt_input[i])) == 0);
-
-      fprintf(stderr, "%s: input='%s', output='%s'\n",
-              ok ? "ok" : "ERROR",
-              salt_input[i], salt);
-
-      if (!ok)
-        status = 1;
+          /* Note: strncpy's somewhat odd fill-to-size-with-NULs behavior
+             is specifically wanted in this case.  */
+          strncpy (prev_output, salt, CRYPT_GENSALT_OUTPUT_SIZE);
+        }
     }
 
   return status;
