c0d5123c185b78168288af9de143918081c3a36e
Zack Weinberg <zackw@panix.com>
2017-09-23 21:15:13 -0400

    Add basic documentation for all functions to crypt.h.

--- crypt-base.h
+++ crypt-base.h
@@ -22,44 +22,115 @@
 
 /*HEADER*/
 
-#define CRYPT_OUTPUT_SIZE               (7 + 22 + 31 + 1)
+/* The strings returned by crypt, crypt_r, crypt_rn, and crypt_ra will
+   be no longer than this.  This is NOT the appropriate size to use in
+   allocating the buffer supplied to crypt_rn -- see below.  */
+#define CRYPT_OUTPUT_SIZE (7 + 22 + 31 + 1)
+
+/* The strings returned by crypt_gensalt, crypt_gensalt_rn, and
+   crypt_gensalt_ra will be no longer than this.  This IS the
+   appropriate size to use when allocating the buffer supplied to
+   crypt_gensalt_rn.  */
 #define CRYPT_GENSALT_OUTPUT_SIZE       (7 + 22 + 1)
 
-/* Encrypt at most 8 characters from KEY using salt to perturb DES.  */
-extern char *crypt (const char *__key, const char *__salt)
+/* One-way hash the passphrase PHRASE as specified by SETTING, and
+   return a string suitable for storage in a Unix-style "passwd" file.
+
+   If SETTING is a previously hashed passphrase, the string returned
+   will be equal to SETTING if and only if PHRASE is the same as the
+   passphrase that was previously hashed.  See the documentation for
+   other ways to use this function.
+
+   The string returned by this function is stored in a statically-
+   allocated buffer, and will be overwritten if the function is called
+   again.  It is not safe to call this function from multiple threads
+   concurrently.
+
+   If an error occurs (such as SETTING being nonsense or unsupported)
+   the string returned will begin with '*', and will not be equal to
+   SETTING nor to any valid hashed passphrase.  Otherwise, the string
+   will not begin with '*'.  */
+extern char *crypt (const char *__phrase, const char *__setting)
   __THROW __nonnull ((1, 2));
 
-/* Reentrant versions of the functions above.  The additional argument
-   points to a structure where the results are placed in.  For the moment,
-   we are preserving the public layout and original size of this struct,
-   but it may change in the future.  */
+/* Memory area used by crypt_r.
+
+   Older versions of this library expected applications to set the
+   'initialized' field of this structure to 0 before calling crypt_r
+   for the first time; this is no longer necessary, but the field is
+   preserved for compatibility's sake.  */
 struct crypt_data
 {
   char opaque[16 * 8 + 32768 * 4 + 14 + 2 + sizeof(long int) + sizeof(int)];
   int initialized;
 };
 
-extern char *crypt_r (const char *__key, const char *__salt,
+/* Thread-safe version of crypt.  Instead of writing to a static
+   storage area, the string returned by this function will be
+   somewhere within the crypt_data object supplied as an argument.
+   Otherwise, behaves exactly the same as crypt.  */
+extern char *crypt_r (const char *__phrase, const char *__setting,
                       struct crypt_data *restrict __data)
   __THROW __nonnull ((1, 2, 3));
 
-extern char *crypt_rn (const char *__key, const char *__salt,
+/* Another thread-safe version of crypt.  Instead of writing to a
+   static storage area, the string returned by this function will be
+   somewhere within the space provided at DATA, which is of length SIZE
+   bytes.  SIZE must be at least sizeof (struct crypt_data).
+
+   Also, if an error occurs, this function returns a null pointer,
+   not a special string.  (However, the string returned on success
+   still will never begin with '*'.)  */
+extern char *crypt_rn (const char *__phrase, const char *__setting,
                        void *__data, int __size)
   __THROW __nonnull ((1, 2, 3));
 
-extern char *crypt_ra (const char *__key, const char *__salt,
+/* Yet a third thread-safe version of crypt; this one works like
+   getline(3).  *DATA must be either NULL or a pointer to memory
+   allocated by malloc, and *SIZE must be the size of the allocation.
+   This space will be allocated or reallocated as necessary and the
+   values updated.  The string returned by this function will be
+   somewhere within the space at *DATA.  It is safe to deallocate
+   this space with free when it is no longer needed.
+
+   Like crypt_rn, this function returns a null pointer on failure, not
+   a special string.  */
+extern char *crypt_ra (const char *__phrase, const char *__setting,
                        void **__data, int *__size)
   __THROW __nonnull ((1, 2, 3, 4));
 
+
+/* Generate a string suitable for use as the setting when hashing a
+   new passphrase.  PREFIX controls which hash function will be used,
+   COUNT controls the computational cost of the hash (for functions
+   where this is tunable), and INPUT should point to SIZE bytes of
+   random data.
+
+   The string returned is stored in a statically-allocated buffer,
+   and will be overwritten if the function is called again.  It is not
+   safe to call this function from multiple threads concurrently.
+   However, it is safe to pass the string to crypt without copying it
+   first; the two functions use separate buffers.
+
+   If an error occurs (e.g. a prefix that does not correspond to a
+   supported hash function, or an inadequate amount of random data),
+   this function returns a null pointer.  */
 extern char *crypt_gensalt (const char *__prefix, unsigned long __count,
                             const char *__input, int __size)
   __THROW __nonnull ((1, 3));
 
+/* Thread-safe version of crypt_gensalt; instead of a
+   statically-allocated buffer, the generated setting string is
+   written to OUTPUT, which is OUTPUT_SIZE bytes long.  OUTPUT_SIZE
+   must be at least CRYPT_GENSALT_OUTPUT_SIZE (see above).  */
 extern char *crypt_gensalt_rn (const char *__prefix, unsigned long __count,
                                const char *__input, int __size,
                                char *__output, int __output_size)
   __THROW __nonnull ((1, 5));
 
+/* Another thread-safe version of crypt_gensalt; the string returned
+   is in storage allocated by malloc, and should be deallocated with
+   free when it is no longer needed.  */
 extern char *crypt_gensalt_ra (const char *__prefix, unsigned long __count,
                                const char *__input, int __size)
   __THROW __nonnull ((1));
