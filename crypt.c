0f1b06f757cd8bad012381c5e9b60183681fb4e6
Björn Esser <besser82@fedoraproject.org>
2017-10-19 15:49:03 +0200

    crypt: Avoid unnecessary shift of memory offset for alignment

--- crypt.c
+++ crypt.c
@@ -56,7 +56,7 @@ get_internal (struct crypt_data *data)
 {
   uintptr_t internalp = (uintptr_t) data->internal;
   const uintptr_t align = alignof (struct crypt_internal);
-  internalp = (internalp + align) & ~(align - 1);
+  internalp = (internalp + align - 1) & ~(align - 1);
   return (struct crypt_internal *)internalp;
 }
 
