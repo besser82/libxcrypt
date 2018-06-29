942d51bbe41f6f84138916c2b180b5b669d65b27
Dmitry V. Levin <ldv@altlinux.org>
2018-06-29 11:07:48 +0000

    Include crypt-port.h in two remaining source files
    crypt-port.h must be the first header file included by each source file,
    this guarantees that config.h is included before any system header.

--- test-crypt-badsalt.c
+++ test-crypt-badsalt.c
@@ -16,6 +16,7 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
+#include "crypt-port.h"
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
