besser82/libxcrypt

commit a7f9df50cecec46bb8176382faa685ce35ca72be

Make salt validation pickier.
This started out as a patch to fold together test-crypt-badsalt.c and
test-crypt-nonnull.c (which were almost the same program) and extend
their testing from DES to all of the supported hashes.  That revealed
that many of the supported hash functions do not validate the contents
of their salt strings very carefully.

This patch has a low but nonzero backward compatibility risk, because
now we reject certain calls to crypt*() that we would previously have
accepted.  In particular, setting strings of the form

    $5$xxxxxxx*xxxxxxxx$

where x stands for any "itoa64" character and * for any non-"itoa64"
character, would formerly be accepted but are now rejected.  Some of
the hash algorithms that were lenient about the contents of the salt
would echo back the salt verbatim, and others would convert it to the
base64 alphabet somehow.  I think it's unlikely that this occurs in
real password files but I don't have a lot of data to base that on.

Makefile.am
@@ -122,11 +122,11 @@ endif
 check_PROGRAMS = \
 	test-alg-des test-alg-hmac-sha1 test-alg-md4 test-alg-md5 \
 	test-alg-sha1 test-alg-sha256 test-alg-sha512 \
-	test-crypt-badsalt test-crypt-bcrypt test-crypt-des \
-	test-crypt-md5 test-crypt-nonnull test-crypt-nthash \
+	test-crypt-bcrypt test-crypt-des \
+	test-crypt-md5 test-crypt-nthash \
 	test-crypt-pbkdf1-sha1 test-crypt-sha256 test-crypt-sha512 \
 	test-crypt-sunmd5 \
-	test-byteorder test-gensalt
+	test-byteorder test-badsalt test-gensalt
 
 if ENABLE_OBSOLETE_API
 libcrypt_la_SOURCES += crypt-des-obsolete.c
@@ -157,16 +157,15 @@ LOG_COMPILER = m4/skip-if-exec-format-error
 endif
 EXTRA_DIST += m4/skip-if-exec-format-error
 
-test_crypt_badsalt_LDADD = libcrypt.la
 test_crypt_bcrypt_LDADD = libcrypt.la
 test_crypt_des_LDADD = libcrypt.la
 test_crypt_md5_LDADD = libcrypt.la
-test_crypt_nonnull_LDADD = libcrypt.la
 test_crypt_nthash_LDADD = libcrypt.la
 test_crypt_pbkdf1_sha1_LDADD = libcrypt.la
 test_crypt_sha256_LDADD = libcrypt.la
 test_crypt_sha512_LDADD = libcrypt.la
 test_crypt_sunmd5_LDADD = libcrypt.la
+test_badsalt_LDADD = libcrypt.la
 test_gensalt_LDADD = libcrypt.la
 test_des_obsolete_LDADD = libcrypt.la
 test_des_obsolete_r_LDADD = libcrypt.la

commit 15d1e3bf3d259ea262f3d34e8c225a3abfabaa09

Add more tests based on gaps in line coverage.
We were not adequately testing invalid-argument error paths within
crypt_gensalt, and we were also not adequately testing a variety of
corner cases related to non-default round parameters

After this change, I believe that all of the non-covered lines fall
into three categories: genuinely impossible to hit from the public
API (usually these are double-checks for preconditions already
verified at a higher level); checks for memory allocation failure; and
fallback paths within get_random_bytes.  The first two of these, IMHO,
are fine to leave as is.  It would probably be worth writing a test
that exercises the get_random_bytes fallbacks but it'd involve getting
one's hands dirty with ELF.  I may do it eventually if no one else does.

This change makes us pickier about non-default round parameters to $5$
and $6$ hashes; numbers outside the valid range are now rejected, as
are numbers with leading zeroes and an explicit request for the
default number of rounds.  This is in keeping with the observation, in
the Passlib documentation, that allowing more than one valid crypt
output string for any given (rounds, salt, phrase) triple is asking
for trouble.

This partially reverts commit
a7f9df50cecec46bb8176382faa685ce35ca72be, which accidentally caused us
to fail to reject invalid DES setting strings.  Because of this, a few
lines in crypt-des.c cease to be covered; they are in category
1 (double-checks of preconditions).

Makefile.am
@@ -126,7 +126,8 @@ check_PROGRAMS = \
 	test-crypt-md5 test-crypt-nthash \
 	test-crypt-pbkdf1-sha1 test-crypt-sha256 test-crypt-sha512 \
 	test-crypt-sunmd5 \
-	test-byteorder test-badsalt test-gensalt test-short-outbuf
+	test-byteorder test-badsalt test-badsetting test-gensalt \
+	test-short-outbuf
 
 if ENABLE_OBSOLETE_API
 libcrypt_la_SOURCES += crypt-des-obsolete.c
@@ -166,6 +167,7 @@ test_crypt_sha256_LDADD = libcrypt.la
 test_crypt_sha512_LDADD = libcrypt.la
 test_crypt_sunmd5_LDADD = libcrypt.la
 test_badsalt_LDADD = libcrypt.la
+test_badsetting_LDADD = libcrypt.la
 test_gensalt_LDADD = libcrypt.la
 test_des_obsolete_LDADD = libcrypt.la
 test_des_obsolete_r_LDADD = libcrypt.la

crypt-pbkdf1-sha1.c
@@ -209,7 +209,7 @@ gensalt_sha1_rn (unsigned long count,
      'count' with.  */
   if (nrbytes < 12 + 4)
     {
-      errno = ERANGE;
+      errno = EINVAL;
       return;
     }
 

crypt-sha256.c
@@ -111,7 +111,6 @@ crypt_sha256_rn (const char *phrase, const char *setting,
   size_t cnt;
   /* Default number of rounds.  */
   size_t rounds = ROUNDS_DEFAULT;
-  bool rounds_custom = false;
 
   /* Find beginning of salt string.  The prefix should normally always
      be present.  Just in case it is not.  */
@@ -123,14 +122,27 @@ crypt_sha256_rn (const char *phrase, const char *setting,
       == 0)
     {
       const char *num = salt + sizeof (sha256_rounds_prefix) - 1;
-      char *endp;
-      unsigned long int srounds = strtoul (num, &endp, 10);
-      if (*endp == '$')
+      /* Do not allow an explicit setting of zero rounds, nor of the
+         default number of rounds, nor leading zeroes on the rounds.  */
+      if (!(*num >= '1' && *num <= '9'))
         {
-          salt = endp + 1;
-          rounds = MAX (ROUNDS_MIN, MIN (srounds, ROUNDS_MAX));
-          rounds_custom = true;
+          errno = EINVAL;
+          return;
         }
+
+      errno = 0;
+      char *endp;
+      rounds = strtoul (num, &endp, 10);
+      if (endp == num || *endp != '$'
+          || rounds < ROUNDS_MIN
+          || rounds > ROUNDS_MAX
+          || rounds == ROUNDS_DEFAULT
+          || errno)
+        {
+          errno = EINVAL;
+          return;
+        }
+      salt = endp + 1;
     }
 
   salt_len = strspn (salt, b64t);
@@ -243,7 +255,7 @@ crypt_sha256_rn (const char *phrase, const char *setting,
   memcpy (cp, sha256_salt_prefix, sizeof (sha256_salt_prefix) - 1);
   cp += sizeof (sha256_salt_prefix) - 1;
 
-  if (rounds_custom)
+  if (rounds != ROUNDS_DEFAULT)
     {
       int n = snprintf (cp,
                         SHA256_HASH_LENGTH - (sizeof (sha256_salt_prefix) - 1),

crypt-sha512.c
@@ -111,7 +111,6 @@ crypt_sha512_rn (const char *phrase, const char *setting,
   size_t cnt;
   /* Default number of rounds.  */
   size_t rounds = ROUNDS_DEFAULT;
-  bool rounds_custom = false;
 
   /* Find beginning of salt string.  The prefix should normally always
      be present.  Just in case it is not.  */
@@ -123,14 +122,27 @@ crypt_sha512_rn (const char *phrase, const char *setting,
       == 0)
     {
       const char *num = salt + sizeof (sha512_rounds_prefix) - 1;
-      char *endp;
-      unsigned long int srounds = strtoul (num, &endp, 10);
-      if (*endp == '$')
+      /* Do not allow an explicit setting of zero rounds, nor of the
+         default number of rounds, nor leading zeroes on the rounds.  */
+      if (!(*num >= '1' && *num <= '9'))
         {
-          salt = endp + 1;
-          rounds = MAX (ROUNDS_MIN, MIN (srounds, ROUNDS_MAX));
-          rounds_custom = true;
+          errno = EINVAL;
+          return;
         }
+
+      errno = 0;
+      char *endp;
+      rounds = strtoul (num, &endp, 10);
+      if (endp == num || *endp != '$'
+          || rounds < ROUNDS_MIN
+          || rounds > ROUNDS_MAX
+          || rounds == ROUNDS_DEFAULT
+          || errno)
+        {
+          errno = EINVAL;
+          return;
+        }
+      salt = endp + 1;
     }
 
   salt_len = strspn (salt, b64t);
@@ -246,7 +258,7 @@ crypt_sha512_rn (const char *phrase, const char *setting,
   memcpy (cp, sha512_salt_prefix, sizeof (sha512_salt_prefix) - 1);
   cp += sizeof (sha512_salt_prefix) - 1;
 
-  if (rounds_custom)
+  if (rounds != ROUNDS_DEFAULT)
     {
       int n = snprintf (cp,
                         SHA512_HASH_LENGTH - (sizeof (sha512_salt_prefix) - 1),

crypt-sunmd5.c
@@ -282,12 +282,12 @@ gensalt_sunmd5_rn (unsigned long count,
                    uint8_t *output,
                    size_t o_size)
 {
-  if (o_size < SUNMD5_MAX_SETTING_LEN + 1 || nrbytes < 6 + 2)
+  if (o_size < SUNMD5_MAX_SETTING_LEN + 1)
     {
       errno = ERANGE;
       return;
     }
-  if (count > SUNMD5_MAX_ROUNDS)
+  if (count > SUNMD5_MAX_ROUNDS || nrbytes < 6 + 2)
     {
       errno = EINVAL;
       return;

crypt.c
@@ -83,6 +83,17 @@ static const struct hashfn hash_algorithms[] =
   HASH_ALGORITHM_TABLE_ENTRIES
 };
 
+#if INCLUDE_des || INCLUDE_des_big
+static int
+is_des_salt_char (char c)
+{
+  return ((c >= 'a' && c <= 'z') ||
+          (c >= 'A' && c <= 'Z') ||
+          (c >= '0' && c <= '9') ||
+          c == '.' || c == '/');
+}
+#endif
+
 static const struct hashfn *
 get_hashfn (const char *setting)
 {
@@ -98,7 +109,7 @@ get_hashfn (const char *setting)
       else
         {
           if (setting[0] == '\0' ||
-             (setting[0] != '$' && setting[0] != '*'))
+              (is_des_salt_char (setting[0]) && is_des_salt_char (setting[1])))
             return h;
         }
 #endif

test-badsalt.c
@@ -124,7 +124,13 @@ static const struct testcase testcases[] =
   { "*MD5 (Sun, plain) invalid char",      14, "$md5$:xMeE.at$"            },
   { "MD5 (Sun, rounds)",                   25, "$md5,rounds=123$1xMeE.at$" },
   { "*MD5 (Sun, rounds) invalid char",     25, "$md5,rounds=123$:xMeE.at$" },
-  { "*MD5 (Sun, rounds) invalid rounds",   25, "$md5,rounds=:23$1xMeE.at$" },
+  { "*MD5 (Sun, rounds) invalid rounds 1", 25, "$md5,rounds=:23$1xMeE.at$" },
+  { "*MD5 (Sun, rounds) invalid rounds 2", 25, "$md5,rounds=12:$1xMeE.at$" },
+  { "*MD5 (Sun, rounds) invalid rounds 3", 25, "$md5,rounds:123$1xMeE.at$" },
+  { "*MD5 (Sun, rounds) invalid rounds 4", 22, "$md5,rounds=$1xMeE.at$"    },
+  { "*MD5 (Sun, rounds) invalid rounds 5", 23, "$md5,rounds=0$1xMeE.at$"   },
+  { "*MD5 (Sun, rounds) invalid rounds 6", 25, "$md5,rounds=012$1xMeE.at$" },
+  { "*MD5 (Sun, rounds) invalid rounds 7", 32, "$md5,rounds=4294967295$1xMeE.at$" },
 #endif
 #if INCLUDE_nthash
   { "NTHASH (bare)",                        3, "$3$"                           },
@@ -133,33 +139,51 @@ static const struct testcase testcases[] =
 #if INCLUDE_sha1
   { "HMAC-SHA1",                           27, "$sha1$123$GGXpNqoJvglVTkGU$" },
   { "*HMAC-SHA1 invalid char",             27, "$sha1$123$:GXpNqoJvglVTkGU$" },
-  { "*HMAC-SHA1 invalid rounds",           27, "$sha1$:23$GGXpNqoJvglVTkGU$" },
+  { "*HMAC-SHA1 invalid rounds 1",         27, "$sha1$:23$GGXpNqoJvglVTkGU$" },
+  { "*HMAC-SHA1 invalid rounds 2",         27, "$sha1$12:$GGXpNqoJvglVTkGU$" },
+  { "*HMAC-SHA1 invalid rounds 3",         27, "$sha1$12:$GGXpNqoJvglVTkGU$" },
 #endif
 #if INCLUDE_sha256
   { "SHA-256 (plain)",                     20, "$5$MJHnaAkegEVYHsFK$"             },
   { "*SHA-256 (plain) invalid char",       20, "$5$:JHnaAkegEVYHsFK$"             },
   { "SHA-256 (rounds)",                    32, "$5$rounds=1000$MJHnaAkegEVYHsFK$" },
-  { "*SHA-256 (rounds) invalid rounds",    32, "$5$rounds=:000$MJHnaAkegEVYHsFK$" },
+  { "*SHA-256 (rounds) invalid rounds 1",  32, "$5$rounds=:000$MJHnaAkegEVYHsFK$" },
+  { "*SHA-256 (rounds) invalid rounds 2",  32, "$5$rounds=100:$MJHnaAkegEVYHsFK$" },
+  { "*SHA-256 (rounds) invalid rounds 3",  32, "$5$rounds:1000$MJHnaAkegEVYHsFK$" },
+  { "*SHA-256 (rounds) invalid rounds 4",  28, "$5$rounds=$MJHnaAkegEVYHsFK$" },
+  { "*SHA-256 (rounds) invalid rounds 5",  29, "$5$rounds=0$MJHnaAkegEVYHsFK$" },
+  { "*SHA-256 (rounds) invalid rounds 6",  32, "$5$rounds=0100$MJHnaAkegEVYHsFK$" },
+  { "*SHA-256 (rounds) invalid rounds 7",  38, "$5$rounds=4294967295$MJHnaAkegEVYHsFK$" },
 #endif
 #if INCLUDE_sha512
   { "SHA-512 (plain)",                     20, "$6$MJHnaAkegEVYHsFK$"             },
   { "*SHA-512 (plain) invalid char",       20, "$6$:JHnaAkegEVYHsFK$"             },
   { "SHA-512 (rounds)",                    32, "$6$rounds=1000$MJHnaAkegEVYHsFK$" },
-  { "*SHA-512 (rounds) invalid rounds",    32, "$6$rounds=:000$MJHnaAkegEVYHsFK$" },
+  { "*SHA-512 (rounds) invalid rounds 1",  32, "$6$rounds=:000$MJHnaAkegEVYHsFK$" },
+  { "*SHA-512 (rounds) invalid rounds 2",  32, "$6$rounds=100:$MJHnaAkegEVYHsFK$" },
+  { "*SHA-512 (rounds) invalid rounds 3",  32, "$6$rounds:1000$MJHnaAkegEVYHsFK$" },
+  { "*SHA-512 (rounds) invalid rounds 4",  28, "$6$rounds=$MJHnaAkegEVYHsFK$" },
+  { "*SHA-512 (rounds) invalid rounds 5",  29, "$6$rounds=0$MJHnaAkegEVYHsFK$" },
+  { "*SHA-512 (rounds) invalid rounds 6",  32, "$6$rounds=0100$MJHnaAkegEVYHsFK$" },
+  { "*SHA-512 (rounds) invalid rounds 6",  38, "$6$rounds=4294967295$MJHnaAkegEVYHsFK$" },
 #endif
 #if INCLUDE_bcrypt
   { "bcrypt (a04)",                        29, "$2a$04$UBVLHeMpJ/QQCv3XqJx8zO" },
   { "*bcrypt (a04) invalid char",          29, "$2a$04$:BVLHeMpJ/QQCv3XqJx8zO" },
-  { "*bcrypt (a04) invalid rounds",        29, "$2a$:4$UBVLHeMpJ/QQCv3XqJx8zO" },
+  { "*bcrypt (a04) invalid rounds 1",      29, "$2a$:4$UBVLHeMpJ/QQCv3XqJx8zO" },
+  { "*bcrypt (a04) invalid rounds 2",      29, "$2a$0:$UBVLHeMpJ/QQCv3XqJx8zO" },
   { "bcrypt (b04)",                        29, "$2b$04$UBVLHeMpJ/QQCv3XqJx8zO" },
   { "*bcrypt (b04) invalid char",          29, "$2b$04$:BVLHeMpJ/QQCv3XqJx8zO" },
-  { "*bcrypt (b04) invalid rounds",        29, "$2b$:4$UBVLHeMpJ/QQCv3XqJx8zO" },
+  { "*bcrypt (b04) invalid rounds 1",      29, "$2b$:4$UBVLHeMpJ/QQCv3XqJx8zO" },
+  { "*bcrypt (b04) invalid rounds 2",      29, "$2b$0:$UBVLHeMpJ/QQCv3XqJx8zO" },
   { "bcrypt (x04)",                        29, "$2x$04$UBVLHeMpJ/QQCv3XqJx8zO" },
   { "*bcrypt (x04) invalid char",          29, "$2x$04$:BVLHeMpJ/QQCv3XqJx8zO" },
-  { "*bcrypt (x04) invalid rounds",        29, "$2x$:4$UBVLHeMpJ/QQCv3XqJx8zO" },
+  { "*bcrypt (x04) invalid rounds 1",      29, "$2x$:4$UBVLHeMpJ/QQCv3XqJx8zO" },
+  { "*bcrypt (x04) invalid rounds 2",      29, "$2x$0:$UBVLHeMpJ/QQCv3XqJx8zO" },
   { "bcrypt (y04)",                        29, "$2y$04$UBVLHeMpJ/QQCv3XqJx8zO" },
   { "*bcrypt (y04) invalid char",          29, "$2y$04$:BVLHeMpJ/QQCv3XqJx8zO" },
-  { "*bcrypt (y04) invalid rounds",        29, "$2y$:4$UBVLHeMpJ/QQCv3XqJx8zO" },
+  { "*bcrypt (y04) invalid rounds 1",      29, "$2y$:4$UBVLHeMpJ/QQCv3XqJx8zO" },
+  { "*bcrypt (y04) invalid rounds 2",      29, "$2y$0:$UBVLHeMpJ/QQCv3XqJx8zO" },
 #endif
 };
 

test-badsetting.c
@@ -1,0 +1,236 @@
+/* Test rejection of ill-formed setting strings.
+
+   Written by Zack Weinberg <zackw at panix.com> in 2017.
+   To the extent possible under law, Zack Weinberg has waived all
+   copyright and related or neighboring rights to this work.
+
+   See https://creativecommons.org/publicdomain/zero/1.0/ for further
+   details.  */
+
+#include "crypt-port.h"
+#include <crypt.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Supply 64 bytes of "random" data to each gensalt call, for
+   determinism.  */
+static const char rbytes[] =
+  "yC8S8E7o+tmofM3L3DgKRwBy+RjWygAXIda7CAghZeXR9ZSl0UZh3kvt2XHg+aKo";
+
+struct testcase
+{
+  const char *prefix;
+  unsigned long count;
+  int rbytes;  /* 0 = use sizeof rbytes - 1 */
+  int osize;   /* 0 = use CRYPT_GENSALT_OUTPUT_SIZE */
+};
+
+/* Note: because all of the test strings below are invalid settings,
+   they can all be tested unconditionally -- it doesn't matter whether
+   the specific hash algorithm that they are invalid for is actually
+   configured in.  */
+
+static const struct testcase testcases[] = {
+  /* DES (traditional and/or bigcrypt) -- count is ignored */
+  { "!a", 0, 0, 0 },            // invalid first character
+  { "a!", 0, 0, 0 },            // invalid second character
+  { "xx", 1, 0, 0 },            // doesn't accept variable counts
+  { "xx", 0, 1, 0 },            // inadequate rbytes
+  { "xx", 0, 0, 1 },            // inadequate osize
+
+
+  /* BSDi extended DES  */
+  { "_", 2,        0, 0 },      // even number
+  { "_", 16777217, 0, 0 },      // too large
+  { "_", 0,        2, 0 },      // inadequate rbytes
+  { "_", 0,        0, 4 },      // inadequate osize
+
+
+  /* MD5 (FreeBSD) */
+  { "$1",  0, 0, 0 },           // truncated prefix
+  { "$1$", 1, 0, 0 },           // doesn't accept variable counts
+  { "$1$", 0, 2, 0 },           // inadequate rbytes
+  { "$1$", 0, 0, 4 },           // inadequate osize
+
+  /* MD5 (Sun) */
+  { "$m",   0,          0, 0 }, // truncated prefix
+  { "$md",  0,          0, 0 },
+  { "$md5", 4294963200, 0, 0 }, // too large
+  { "$md5", 0,          2, 0 }, // inadequate rbytes
+  { "$md5", 0,          0, 4 }, // inadequate osize
+
+  /* NTHASH */
+  { "$3",  0, 0, 0 },           // truncated prefix
+  { "$3$", 0, 0, 4 },           // inadequate osize
+
+  /* SHA1 */
+  { "$s",   0, 0, 0 },          // truncated prefix
+  { "$sh",  0, 0, 0 },
+  { "$sha", 0, 0, 0 },
+  { "$sha1", 0, 2, 0 },         // inadequate rbytes
+  { "$sha1", 0, 0, 4 },         // inadequate osize
+
+  /* SHA256 */
+  { "$5",  0,          0, 0 },  // truncated prefix
+  { "$5$", 999,        0, 0 },  // too small
+  { "$5$", 1000000000, 0, 0 },  // too large
+  { "$5$", 0,          2, 0 },  // inadequate rbytes
+  { "$5$", 0,          0, 4 },  // inadequate osize
+
+  /* SHA512 */
+  { "$6",  0,          0, 0 },  // truncated prefix
+  { "$6$", 999,        0, 0 },  // too small
+  { "$6$", 1000000000, 0, 0 },  // too large
+  { "$6$", 0,          2, 0 },  // inadequate rbytes
+  { "$6$", 0,          0, 4 },  // inadequate osize
+
+  /* bcrypt */
+  { "$2",   0,  0, 0 },         // truncated prefix
+  { "$2a",  0,  0, 0 },
+  { "$2b",  0,  0, 0 },
+  { "$2x",  0,  0, 0 },
+  { "$2y",  0,  0, 0 },
+  { "$2b$", 3,  0, 0 },         // too small
+  { "$2b$", 32, 0, 0 },         // too large
+  { "$2b$", 0,  2, 0 },         // inadequate rbytes
+  { "$2b$", 0,  0, 4 },         // inadequate osize
+};
+
+static void
+print_escaped_string (const char *s)
+{
+  putchar ('"');
+  for (const char *p = s; *p; p++)
+    if (*p >= ' ' && *p <= '~')
+      {
+        if (*p == '\\' || *p == '\"')
+          putchar ('\\');
+        putchar (*p);
+      }
+    else
+      printf ("\\x%02x", (unsigned int)(unsigned char)*p);
+  putchar ('"');
+}
+
+static bool error_occurred = false;
+static void
+report_error (const char *fn, const struct testcase *tc,
+              int err, const char *output)
+{
+  error_occurred = true;
+  printf ("%s(", fn);
+  print_escaped_string (tc->prefix);
+  printf (", %lu, nrbytes=%d, osize=%d):\n", tc->count,
+          tc->rbytes > 0 ? tc->rbytes : (int) sizeof rbytes - 1,
+          tc->osize  > 0 ? tc->osize  : CRYPT_GENSALT_OUTPUT_SIZE);
+
+  if (output)
+    {
+      if (err)
+        printf ("\toutput with errno = %s\n", strerror (err));
+      printf ("\texpected NULL, got ");
+      print_escaped_string (output);
+      putchar ('\n');
+    }
+  else if (err != (tc->osize > 0 ? ERANGE : EINVAL))
+    printf ("\tno output with errno = %s\n",
+            err ? strerror (err) : "0");
+  else
+    printf ("\tno output with errno = %s"
+            "(shouldn't have been called)\n", strerror (err));
+  putchar ('\n');
+}
+
+static void
+test_one (const struct testcase *tc)
+{
+  char obuf[CRYPT_GENSALT_OUTPUT_SIZE];
+  char *s;
+  int nrbytes = tc->rbytes > 0 ? tc->rbytes : (int)(sizeof rbytes - 1);
+  int osize   = tc->osize  > 0 ? tc->osize : CRYPT_GENSALT_OUTPUT_SIZE;
+
+  /* It is only possible to provide a variant osize to crypt_gensalt_rn.  */
+  if (tc->osize == 0)
+    {
+      errno = 0;
+      s = crypt_gensalt (tc->prefix, tc->count, rbytes, nrbytes);
+      if (s || errno != EINVAL)
+        report_error ("gensalt", tc, errno, s);
+
+      errno = 0;
+      s = crypt_gensalt_ra (tc->prefix, tc->count, rbytes, nrbytes);
+      if (s || errno != EINVAL)
+        report_error ("gensalt_ra", tc, errno, s);
+      free (s);
+    }
+
+  errno = 0;
+  s = crypt_gensalt_rn (tc->prefix, tc->count, rbytes, nrbytes, obuf, osize);
+  if (s || errno != (tc->osize > 0 ? ERANGE : EINVAL))
+    report_error ("gensalt_rn", tc, errno, s);
+}
+
+/* All single-character strings (except "_" when BSDi extended DES
+   is enabled) are invalid prefixes, either because the character
+   cannot be the first character of any valid prefix, or because the
+   string is too short.  */
+static void
+test_single_characters (void)
+{
+  char s[2];
+  struct testcase tc;
+  s[1] = '\0';
+  tc.prefix = s;
+  tc.count = 0;
+  tc.rbytes = 0;
+  tc.osize = 0;
+
+  for (int i = 1; i < 256; i++)
+    {
+#ifdef INCLUDE_des_xbsd
+      if (i == '_') continue;
+#endif
+      s[0] = (char)i;
+      test_one (&tc);
+    }
+}
+
+/* '$' followed by any non-ASCII-isalnum character is also always
+   invalid.  */
+static void
+test_dollar_nonalphanum (void)
+{
+  char s[3];
+  struct testcase tc;
+  s[0] = '$';
+  s[2] = '\0';
+  tc.prefix = s;
+  tc.count = 0;
+  tc.rbytes = 0;
+  tc.osize = 0;
+
+  for (int i = 1; i < 256; i++)
+    {
+      if (('0' >= i && i <= '9') ||
+          ('A' >= i && i <= 'Z') ||
+          ('a' >= i && i <= 'z'))
+        continue;
+      s[1] = (char)i;
+      test_one (&tc);
+    }
+}
+
+int
+main(void)
+{
+  test_single_characters();
+  test_dollar_nonalphanum();
+
+  /* Hand-crafted arguments for each supported algorithm.  */
+  for (size_t i = 0; i < ARRAY_SIZE (testcases); i++)
+    test_one (&testcases[i]);
+
+  return error_occurred;
+}

test-crypt-sha256.c
@@ -22,11 +22,6 @@ static const struct
     "opqey6IcA"
   },
   {
-    "$5$rounds=5000$toolongsaltstring", "This is just a test",
-    "$5$rounds=5000$toolongsaltstrin$Un/5jzAHMgOGZ5.mWJpuVolil07guHPvOW8"
-    "mGRcvxa5"
-  },
-  {
     "$5$rounds=1400$anotherlongsaltstring",
     "a very much longer text to encrypt.  This one even stretches over more"
     "than one line.",
@@ -42,11 +37,6 @@ static const struct
     "$5$rounds=123456$asaltof16chars..", "a short string",
     "$5$rounds=123456$asaltof16chars..$gP3VQ/6X7UUEW3HkBn2w1/Ptq2jxPyzV/"
     "cZKmF/wJvD"
-  },
-  {
-    "$5$rounds=10$roundstoolow", "the minimum number is still observed",
-    "$5$rounds=1000$roundstoolow$yfvwcWrQ8l/K0DAWyuPMDNHpIVlTQebY9l/gL97"
-    "2bIC"
   },
 };
 

test-crypt-sha512.c
@@ -23,11 +23,6 @@ static const struct
     "HbbMCVNSnCM/UrjmM0Dp8vOuZeHBy/YTBmSK6H9qs/y3RnOaw5v."
   },
   {
-    "$6$rounds=5000$toolongsaltstring", "This is just a test",
-    "$6$rounds=5000$toolongsaltstrin$lQ8jolhgVRVhY4b5pZKaysCLi0QBxGoNeKQ"
-    "zQ3glMhwllF7oGDZxUhx1yxdYcz/e1JSbq3y6JMxxl8audkUEm0"
-  },
-  {
     "$6$rounds=1400$anotherlongsaltstring",
     "a very much longer text to encrypt.  This one even stretches over more"
     "than one line.",
@@ -44,11 +39,6 @@ static const struct
     "$6$rounds=123456$asaltof16chars..", "a short string",
     "$6$rounds=123456$asaltof16chars..$BtCwjqMJGx5hrJhZywWvt0RLE8uZ4oPwc"
     "elCjmw2kSYu.Ec6ycULevoBK25fs2xXgMNrCzIMVcgEJAstJeonj1"
-  },
-  {
-    "$6$rounds=10$roundstoolow", "the minimum number is still observed",
-    "$6$rounds=1000$roundstoolow$kUMsbe306n21p9R.FRkW3IGn.S9NPN0x50YhH1x"
-    "hLsPuWGsUSklZt58jaTfF4ZEQpyUNGc0dqbpBYYBaHHrsX."
   },
 };
 #define ntests ARRAY_SIZE (tests)

test-gensalt.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 static const char *const entropy[] =
 {
@@ -24,6 +25,13 @@ static const char *const bsdi_expected_output[] =
   "_J9..PKXc",
   "_J9..ZAFl",
   "_J9..UqGB"
+};
+static const char *const bsdi_expected_output_r[] =
+{
+  "_DT0.MJHn",
+  "_DT0.PKXc",
+  "_DT0.ZAFl",
+  "_DT0.UqGB"
 };
 #endif
 #if INCLUDE_md5
@@ -52,14 +60,28 @@ static const char *const sunmd5_expected_output[] =
   "$md5,rounds=42259$3HtkHq/x$",
   "$md5,rounds=73773$p.5e9AQf$",
 };
+static const char *const sunmd5_expected_output_r[] =
+{
+  "$md5,rounds=4294920244$BPm.fm03$",
+  "$md5,rounds=4294937396$WKoucttX$",
+  "$md5,rounds=4294907154$3HtkHq/x$",
+  "$md5,rounds=4294938668$p.5e9AQf$",
+};
 #endif
 #if INCLUDE_sha1
-static const char *const pbkdf_expected_output[] =
+static const char *const sha1_expected_output[] =
 {
   "$sha1$248488$ggu.H673kaZ5$",
   "$sha1$248421$SWqudaxXA5L0$",
   "$sha1$257243$RAtkIrDxEovH$",
   "$sha1$250464$1j.eVxRfNAPO$",
+};
+static const char *const sha1_expected_output_r[] =
+{
+  "$sha1$3643984551$ggu.H673kaZ5$",
+  "$sha1$4200450659$SWqudaxXA5L0$",
+  "$sha1$3946507480$RAtkIrDxEovH$",
+  "$sha1$3486175838$1j.eVxRfNAPO$",
 };
 #endif
 #if INCLUDE_sha256
@@ -70,6 +92,13 @@ static const char *const sha256_expected_output[] =
   "$5$ZAFlICwYRETzIzIj",
   "$5$UqGBkVu01rurVZqg"
 };
+static const char *const sha256_expected_output_r[] =
+{
+  "$5$rounds=10191$MJHnaAkegEVYHsFK",
+  "$5$rounds=10191$PKXc3hCOSyMqdaEQ",
+  "$5$rounds=10191$ZAFlICwYRETzIzIj",
+  "$5$rounds=10191$UqGBkVu01rurVZqg"
+};
 #endif
 #if INCLUDE_sha512
 static const char *const sha512_expected_output[] =
@@ -78,6 +107,13 @@ static const char *const sha512_expected_output[] =
   "$6$PKXc3hCOSyMqdaEQ",
   "$6$ZAFlICwYRETzIzIj",
   "$6$UqGBkVu01rurVZqg"
+};
+static const char *const sha512_expected_output_r[] =
+{
+  "$6$rounds=10191$MJHnaAkegEVYHsFK",
+  "$6$rounds=10191$PKXc3hCOSyMqdaEQ",
+  "$6$rounds=10191$ZAFlICwYRETzIzIj",
+  "$6$rounds=10191$UqGBkVu01rurVZqg"
 };
 #endif
 #if INCLUDE_bcrypt
@@ -117,41 +153,57 @@ struct testcase
   const char *const *expected_output;
   unsigned int expected_len;
   unsigned int expected_auto_len;
+  unsigned long rounds;
 };
 
 static const struct testcase testcases[] =
 {
 #if INCLUDE_des || INCLUDE_des_big
-  { "",      des_expected_output,       2,  0 }, // DES
+  { "",      des_expected_output,       2,  0, 0 },
+  // DES doesn't have variable round count.
 #endif
 #if INCLUDE_des_xbsd
-  { "_",     bsdi_expected_output,      9,  0 }, // BSDi extended DES
+  { "_",     bsdi_expected_output,      9,  0, 0 },
+  { "_",     bsdi_expected_output_r,    9,  0, 10191 },
 #endif
 #if INCLUDE_md5
-  { "$1$",   md5_expected_output,      11,  0 }, // MD5
+  { "$1$",   md5_expected_output,      11,  0, 0 },
+  // MD5/BSD doesn't have variable round count.
 #endif
 #if INCLUDE_nthash
-  { "$3$",   nthash_expected_output,   29,  0 }, // NTHASH
+  { "$3$",   nthash_expected_output,   29,  0, 0 },
+  // NTHASH doesn't have variable round count.
 #endif
 #if INCLUDE_sunmd5
-  { "$md5",  sunmd5_expected_output,   27,  0 }, // SUNMD5
+  { "$md5",  sunmd5_expected_output,   27,  0, 0 },
+  // SHA1/PBKDF always emits a round count, but we need to test its
+  // behavior on very large inputs.  (This number is the largest
+  // supported round count.)
+  { "$md5", sunmd5_expected_output_r,  32, 0, 4294963199 },
 #endif
 #if INCLUDE_sha1
-  { "$sha1", pbkdf_expected_output,    26, 34 }, // PBKDF with SHA1
+  { "$sha1", sha1_expected_output,     26, 34, 0 },
+  // SHA1/PBKDF always emits a round count, but we need to test its
+  // behavior on very large inputs.  (The behavior should be the
+  // same whether or not ULONG_MAX > UINT32_MAX.)
+  { "$sha1", sha1_expected_output_r,   30, 38, ULONG_MAX },
 #endif
 #if INCLUDE_sha256
-  { "$5$",   sha256_expected_output,   19,  0 }, // SHA-2-256
+  { "$5$",   sha256_expected_output,   19,  0, 0 },
+  { "$5$",   sha256_expected_output_r, 32,  0, 10191 },
 #endif
 #if INCLUDE_sha512
-  { "$6$",   sha512_expected_output,   19,  0 }, // SHA-2-512
+  { "$6$",   sha512_expected_output,   19,  0, 0 },
+  { "$6$",   sha512_expected_output_r, 32,  0, 10191 },
 #endif
 #if INCLUDE_bcrypt
-  { "$2a$",  bcrypt_a_expected_output, 29,  0 }, // bcrypt mode A
-  { "$2b$",  bcrypt_b_expected_output, 29,  0 }, // bcrypt mode B
-  { "$2x$",  bcrypt_x_expected_output, 29,  0 }, // bcrypt mode X
-  { "$2y$",  bcrypt_y_expected_output, 29,  0 }, // bcrypt mode Y
+  { "$2a$",  bcrypt_a_expected_output, 29,  0, 0 },
+  { "$2b$",  bcrypt_b_expected_output, 29,  0, 0 },
+  { "$2x$",  bcrypt_x_expected_output, 29,  0, 0 },
+  { "$2y$",  bcrypt_y_expected_output, 29,  0, 0 },
+  // bcrypt gensalt always emits a round count.
 #endif
-  { 0, 0, 0, 0 }
+  { 0, 0, 0, 0, 0 }
 };
 
 int
@@ -169,7 +221,7 @@ main (void)
       for (ent = 0; ent < ARRAY_SIZE (entropy); ent++)
         {
           XCRYPT_SECURE_MEMSET (output, CRYPT_GENSALT_OUTPUT_SIZE);
-          char *salt = crypt_gensalt_rn (tcase->prefix, 0,
+          char *salt = crypt_gensalt_rn (tcase->prefix, tcase->rounds,
                                          entropy[ent], 16,
                                          output, CRYPT_GENSALT_OUTPUT_SIZE);
           if (salt == 0)