40a0d88687d7944ae618b1f507e6a0adc532399c
Björn Esser <besser82@fedoraproject.org>
2018-07-14 21:00:10 +0200

    crypt-sha256.c: Apply previous commits from Zack Weinberg
    This patch summarizes the changes from:
    
      * 4a1152b4f9bc900d0488b473e20daed95025f4b0
      * ccc833b4b2c60cefd74cb91a05f1ce7628179657
      * 118abc2c5c08542fe528a1193328b161a756db90
      * 9483c648978378a9cd128382049a88055d5b2a61
      * a67c7cd73c19592315ae184fe8dee155ef6ebd31
      * 4856729bd75c78e4e1e4e35ab5afd7cef6c66afc
      * a2d558581c3cd9b3d62f916825405cebd708f268
      * b46a39a57e4801f5c41b1bac207239b6ed8e31a5
      * cbdf27e254b0a113f0bea87071177f083e3f2545
      * 00ec9369ac4875a8e3903d6e41ae785bbc8e8b26
      * 114d4200eaa572525089795cf7990c3dafd5028c
      * ed4be6afa7b34fe0d827cc3f7f9608de0d9325cd
      * a7f9df50cecec46bb8176382faa685ce35ca72be
      * 15d1e3bf3d259ea262f3d34e8c225a3abfabaa09

--- crypt-sha256.c
+++ crypt-sha256.c
@@ -1,13 +1,15 @@
 /* One way encryption based on the SHA256-based Unix crypt implementation.
  *
  * Written by Ulrich Drepper <drepper at redhat.com> in 2007 [1].
- * To the extent possible under law, Ulrich Drepper has waived all
+ * Modified by Zack Weinberg <zackw at panix.com> in 2017, 2018.
+ * Composed by Björn Esser <besser82 at fedoraproject.org> in 2018.
+ * To the extent possible under law, the named authors have waived all
  * copyright and related or neighboring rights to this work.
  *
  * See https://creativecommons.org/publicdomain/zero/1.0/ for further
  * details.
  *
- * This file is an except from [2], lines 648 up to 909.
+ * This file is a modified except from [2], lines 648 up to 909.
  *
  * [1]  https://www.akkadia.org/drepper/sha-crypt.html
  * [2]  https://www.akkadia.org/drepper/SHA-crypt.txt
@@ -39,8 +41,35 @@ static const char sha256_rounds_prefix[] = "rounds=";
 /* Maximum number of rounds.  */
 #define ROUNDS_MAX 999999999
 
+/* The maximum possible length of a SHA256-hashed password string,
+   including the terminating NUL character.  Prefix (including its NUL)
+   + rounds tag ("rounds=$" = "rounds=\0") + strlen(ROUNDS_MAX)
+   + salt (up to SALT_LEN_MAX chars) + '$' + hash (43 chars).  */
+
+#define LENGTH_OF_NUMBER(n) (sizeof #n - 1)
+
+#define SHA256_HASH_LENGTH \
+  (sizeof (sha256_salt_prefix) + sizeof (sha256_rounds_prefix) + \
+   LENGTH_OF_NUMBER (ROUNDS_MAX) + SALT_LEN_MAX + 1 + 43)
+
+static_assert (SHA256_HASH_LENGTH <= CRYPT_OUTPUT_SIZE,
+               "CRYPT_OUTPUT_SIZE is too small for SHA256");
+
+/* A sha256_buffer holds all of the sensitive intermediate data.  */
+struct sha256_buffer
+{
+  struct sha256_ctx ctx;
+  uint8_t result[32];
+  uint8_t p_bytes[32];
+  uint8_t s_bytes[32];
+};
+
+static_assert (sizeof (struct sha256_buffer) <= ALG_SPECIFIC_SIZE,
+               "ALG_SPECIFIC_SIZE is too small for SHA256");
+
+
 /* Table with characters for base64 transformation.  */
-static const char b64t[64] =
+static const char b64t[] =
   "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 /* Subroutine of _xcrypt_crypt_sha256_rn: Feed CTX with LEN bytes of a
@@ -56,23 +85,30 @@ sha256_process_recycled_bytes (unsigned char block[32], size_t len,
   sha256_process_bytes (block, cnt, ctx);
 }
 
-static char *
-sha256_crypt_r (const char *key, const char *salt, char *buffer, int buflen)
+void
+crypt_sha256_rn (const char *phrase, size_t phr_size,
+                 const char *setting, size_t ARG_UNUSED (set_size),
+                 uint8_t *output, size_t out_size,
+                 void *scratch, size_t scr_size)
 {
-  unsigned char alt_result[32]
-  __attribute__ ((__aligned__ (__alignof__ (uint32_t))));
-  unsigned char temp_result[32]
-  __attribute__ ((__aligned__ (__alignof__ (uint32_t))));
-  struct sha256_ctx ctx;
-  struct sha256_ctx alt_ctx;
-  size_t salt_len;
-  size_t key_len;
+  /* This shouldn't ever happen, but...  */
+  if (out_size < SHA256_HASH_LENGTH
+      || scr_size < sizeof (struct sha256_buffer))
+    {
+      errno = ERANGE;
+      return;
+    }
+
+  struct sha256_buffer *buf = scratch;
+  struct sha256_ctx *ctx = &buf->ctx;
+  uint8_t *result = buf->result;
+  uint8_t *p_bytes = buf->p_bytes;
+  uint8_t *s_bytes = buf->s_bytes;
+  char *cp = (char *)output;
+  const char *salt = setting;
+
+  size_t salt_size;
   size_t cnt;
-  char *cp;
-  char *copied_key = NULL;
-  char *copied_salt = NULL;
-  char *p_bytes;
-  char *s_bytes;
   /* Default number of rounds.  */
   size_t rounds = ROUNDS_DEFAULT;
   bool rounds_custom = false;
@@ -87,214 +123,176 @@ sha256_crypt_r (const char *key, const char *salt, char *buffer, int buflen)
       == 0)
     {
       const char *num = salt + sizeof (sha256_rounds_prefix) - 1;
-      char *endp;
-      unsigned long int srounds = strtoul (num, &endp, 10);
-      if (*endp == '$')
+      /* Do not allow an explicit setting of zero rounds, nor of the
+         default number of rounds, nor leading zeroes on the rounds.  */
+      if (!(*num >= '1' && *num <= '9'))
         {
-          salt = endp + 1;
-          rounds = MAX (ROUNDS_MIN, MIN (srounds, ROUNDS_MAX));
-          rounds_custom = true;
+          errno = EINVAL;
+          return;
         }
+
+      errno = 0;
+      char *endp;
+      rounds = strtoul (num, &endp, 10);
+      if (endp == num || *endp != '$'
+          || rounds < ROUNDS_MIN
+          || rounds > ROUNDS_MAX
+          || errno)
+        {
+          errno = EINVAL;
+          return;
+        }
+      salt = endp + 1;
+      rounds_custom = true;
     }
 
-  salt_len = MIN (strcspn (salt, "$"), SALT_LEN_MAX);
-  key_len = strlen (key);
-
-  if ((key - (char *) 0) % __alignof__ (uint32_t) != 0)
+  salt_size = strspn (salt, b64t);
+  if (salt[salt_size] && salt[salt_size] != '$')
     {
-      char *tmp = (char *) alloca (key_len + __alignof__ (uint32_t));
-      key = copied_key =
-              memcpy (tmp + __alignof__ (uint32_t)
-                      - (tmp - (char *) 0) % __alignof__ (uint32_t),
-                      key, key_len);
+      errno = EINVAL;
+      return;
     }
+  if (salt_size > SALT_LEN_MAX)
+    salt_size = SALT_LEN_MAX;
 
-  if ((salt - (char *) 0) % __alignof__ (uint32_t) != 0)
-    {
-      char *tmp = (char *) alloca (salt_len + __alignof__ (uint32_t));
-      salt = copied_salt =
-               memcpy (tmp + __alignof__ (uint32_t)
-                       - (tmp - (char *) 0) % __alignof__ (uint32_t),
-                       salt, salt_len);
-    }
-
-  /* Prepare for the real work.  */
-  sha256_init_ctx (&ctx);
-
-  /* Add the key string.  */
-  sha256_process_bytes (key, key_len, &ctx);
-
-  /* The last part is the salt string.  This must be at most 16
-     characters and it ends at the first `$' character (for
-     compatibility with existing implementations).  */
-  sha256_process_bytes (salt, salt_len, &ctx);
-
-  /* Compute alternate SHA256 sum with input KEY, SALT, and KEY.  The
+  /* Compute alternate SHA256 sum with input PHRASE, SALT, and PHRASE.  The
      final result will be added to the first context.  */
-  sha256_init_ctx (&alt_ctx);
+  sha256_init_ctx (ctx);
 
-  /* Add key.  */
-  sha256_process_bytes (key, key_len, &alt_ctx);
+  /* Add phrase.  */
+  sha256_process_bytes (phrase, phr_size, ctx);
 
   /* Add salt.  */
-  sha256_process_bytes (salt, salt_len, &alt_ctx);
+  sha256_process_bytes (salt, salt_size, ctx);
 
-  /* Add key again.  */
-  sha256_process_bytes (key, key_len, &alt_ctx);
+  /* Add phrase again.  */
+  sha256_process_bytes (phrase, phr_size, ctx);
 
-  /* Now get result of this (32 bytes) and add it to the other
-     context.  */
-  sha256_finish_ctx (&alt_ctx, alt_result);
+  /* Now get result of this (32 bytes).  */
+  sha256_finish_ctx (ctx, result);
 
-  /* Add for any character in the key one byte of the alternate sum.  */
-  for (cnt = key_len; cnt > 32; cnt -= 32)
-    sha256_process_bytes (alt_result, 32, &ctx);
-  sha256_process_bytes (alt_result, cnt, &ctx);
+  /* Prepare for the real work.  */
+  sha256_init_ctx (ctx);
 
-  /* Take the binary representation of the length of the key and for every
-     1 add the alternate sum, for every 0 the key.  */
-  for (cnt = key_len; cnt > 0; cnt >>= 1)
+  /* Add the phrase string.  */
+  sha256_process_bytes (phrase, phr_size, ctx);
+
+  /* The last part is the salt string.  This must be at most 8
+     characters and it ends at the first `$' character (for
+     compatibility with existing implementations).  */
+  sha256_process_bytes (salt, salt_size, ctx);
+
+  /* Add for any character in the phrase one byte of the alternate sum.  */
+  for (cnt = phr_size; cnt > 32; cnt -= 32)
+    sha256_process_bytes (result, 32, ctx);
+  sha256_process_bytes (result, cnt, ctx);
+
+  /* Take the binary representation of the length of the phrase and for every
+     1 add the alternate sum, for every 0 the phrase.  */
+  for (cnt = phr_size; cnt > 0; cnt >>= 1)
     if ((cnt & 1) != 0)
-      sha256_process_bytes (alt_result, 32, &ctx);
+      sha256_process_bytes (result, 32, ctx);
     else
-      sha256_process_bytes (key, key_len, &ctx);
+      sha256_process_bytes (phrase, phr_size, ctx);
 
   /* Create intermediate result.  */
-  sha256_finish_ctx (&ctx, alt_result);
+  sha256_finish_ctx (ctx, result);
 
   /* Start computation of P byte sequence.  */
-  sha256_init_ctx (&alt_ctx);
+  sha256_init_ctx (ctx);
 
   /* For every character in the password add the entire password.  */
-  for (cnt = 0; cnt < key_len; ++cnt)
-    sha256_process_bytes (key, key_len, &alt_ctx);
+  for (cnt = 0; cnt < phr_size; ++cnt)
+    sha256_process_bytes (phrase, phr_size, ctx);
 
   /* Finish the digest.  */
-  sha256_finish_ctx (&alt_ctx, temp_result);
-
-  /* Create byte sequence P.  */
-  cp = p_bytes = alloca (key_len);
-  for (cnt = key_len; cnt >= 32; cnt -= 32)
-    cp = mempcpy (cp, temp_result, 32);
-  memcpy (cp, temp_result, cnt);
+  sha256_finish_ctx (ctx, p_bytes);
 
   /* Start computation of S byte sequence.  */
-  sha256_init_ctx (&alt_ctx);
+  sha256_init_ctx (ctx);
 
   /* For every character in the password add the entire password.  */
-  for (cnt = 0; cnt < 16 + alt_result[0]; ++cnt)
-    sha256_process_bytes (salt, salt_len, &alt_ctx);
+  for (cnt = 0; cnt < (size_t) 16 + (size_t) result[0]; ++cnt)
+    sha256_process_bytes (salt, salt_size, ctx);
 
   /* Finish the digest.  */
-  sha256_finish_ctx (&alt_ctx, temp_result);
-
-  /* Create byte sequence S.  */
-  cp = s_bytes = alloca (salt_len);
-  for (cnt = salt_len; cnt >= 32; cnt -= 32)
-    cp = mempcpy (cp, temp_result, 32);
-  memcpy (cp, temp_result, cnt);
+  sha256_finish_ctx (ctx, s_bytes);
 
   /* Repeatedly run the collected hash value through SHA256 to burn
      CPU cycles.  */
   for (cnt = 0; cnt < rounds; ++cnt)
     {
       /* New context.  */
-      sha256_init_ctx (&ctx);
+      sha256_init_ctx (ctx);
 
-      /* Add key or last result.  */
+      /* Add phrase or last result.  */
       if ((cnt & 1) != 0)
-        sha256_process_bytes (p_bytes, key_len, &ctx);
+        sha256_process_recycled_bytes (p_bytes, phr_size, ctx);
       else
-        sha256_process_bytes (alt_result, 32, &ctx);
+        sha256_process_bytes (result, 32, ctx);
 
       /* Add salt for numbers not divisible by 3.  */
       if (cnt % 3 != 0)
-        sha256_process_bytes (s_bytes, salt_len, &ctx);
+        sha256_process_recycled_bytes (s_bytes, salt_size, ctx);
 
-      /* Add key for numbers not divisible by 7.  */
+      /* Add phrase for numbers not divisible by 7.  */
       if (cnt % 7 != 0)
-        sha256_process_bytes (p_bytes, key_len, &ctx);
+        sha256_process_recycled_bytes (p_bytes, phr_size, ctx);
 
-      /* Add key or last result.  */
+      /* Add phrase or last result.  */
       if ((cnt & 1) != 0)
-        sha256_process_bytes (alt_result, 32, &ctx);
+        sha256_process_bytes (result, 32, ctx);
       else
-        sha256_process_bytes (p_bytes, key_len, &ctx);
+        sha256_process_recycled_bytes (p_bytes, phr_size, ctx);
 
       /* Create intermediate result.  */
-      sha256_finish_ctx (&ctx, alt_result);
+      sha256_finish_ctx (ctx, result);
     }
 
-  /* Now we can construct the result string.  It consists of three
-     parts.  */
-  cp = stpncpy (buffer, sha256_salt_prefix, MAX (0, buflen));
-  buflen -= sizeof (sha256_salt_prefix) - 1;
+  /* Now we can construct the result string.  It consists of four
+     parts, one of which is optional.  We already know that there
+     is sufficient space at CP for the longest possible result string.  */
+  memcpy (cp, sha256_salt_prefix, sizeof (sha256_salt_prefix) - 1);
+  cp += sizeof (sha256_salt_prefix) - 1;
 
   if (rounds_custom)
     {
-      int n = snprintf (cp, MAX (0, buflen), "%s%zu$",
-                        sha256_rounds_prefix, rounds);
+      int n = snprintf (cp,
+                        SHA256_HASH_LENGTH - (sizeof (sha256_salt_prefix) - 1),
+                        "%s%zu$", sha256_rounds_prefix, rounds);
       cp += n;
-      buflen -= n;
     }
 
-  cp = stpncpy (cp, salt, MIN ((size_t) MAX (0, buflen), salt_len));
-  buflen -= MIN ((size_t) MAX (0, buflen), salt_len);
+  memcpy (cp, salt, salt_size);
+  cp += salt_size;
+  *cp++ = '$';
 
-  if (buflen > 0)
-    {
-      *cp++ = '$';
-      --buflen;
-    }
-
-#define b64_from_24bit(B2, B1, B0, N)					      \
-  do {									      \
-    unsigned int w = ((B2) << 16) | ((B1) << 8) | (B0);			      \
-    int n = (N);							      \
-    while (n-- > 0 && buflen > 0)					      \
-      {									      \
-	*cp++ = b64t[w & 0x3f];						      \
-	--buflen;							      \
-	w >>= 6;							      \
-      }									      \
+#define b64_from_24bit(B2, B1, B0, N)                   \
+  do {                                                  \
+    unsigned int w = ((((unsigned int)(B2)) << 16) |    \
+                      (((unsigned int)(B1)) << 8) |     \
+                      ((unsigned int)(B0)));            \
+    int n = (N);                                        \
+    while (n-- > 0)                                     \
+      {                                                 \
+        *cp++ = b64t[w & 0x3f];                         \
+        w >>= 6;                                        \
+      }                                                 \
   } while (0)
 
-  b64_from_24bit (alt_result[0], alt_result[10], alt_result[20], 4);
-  b64_from_24bit (alt_result[21], alt_result[1], alt_result[11], 4);
-  b64_from_24bit (alt_result[12], alt_result[22], alt_result[2], 4);
-  b64_from_24bit (alt_result[3], alt_result[13], alt_result[23], 4);
-  b64_from_24bit (alt_result[24], alt_result[4], alt_result[14], 4);
-  b64_from_24bit (alt_result[15], alt_result[25], alt_result[5], 4);
-  b64_from_24bit (alt_result[6], alt_result[16], alt_result[26], 4);
-  b64_from_24bit (alt_result[27], alt_result[7], alt_result[17], 4);
-  b64_from_24bit (alt_result[18], alt_result[28], alt_result[8], 4);
-  b64_from_24bit (alt_result[9], alt_result[19], alt_result[29], 4);
-  b64_from_24bit (0, alt_result[31], alt_result[30], 3);
-  if (buflen <= 0)
-    {
-      errno = ERANGE;
-      buffer = NULL;
-    }
-  else
-    *cp = '\0';		/* Terminate the string.  */
+  b64_from_24bit (result[0], result[10], result[20], 4);
+  b64_from_24bit (result[21], result[1], result[11], 4);
+  b64_from_24bit (result[12], result[22], result[2], 4);
+  b64_from_24bit (result[3], result[13], result[23], 4);
+  b64_from_24bit (result[24], result[4], result[14], 4);
+  b64_from_24bit (result[15], result[25], result[5], 4);
+  b64_from_24bit (result[6], result[16], result[26], 4);
+  b64_from_24bit (result[27], result[7], result[17], 4);
+  b64_from_24bit (result[18], result[28], result[8], 4);
+  b64_from_24bit (result[9], result[19], result[29], 4);
+  b64_from_24bit (0, result[31], result[30], 3);
 
-  /* Clear the buffer for the intermediate result so that people
-     attaching to processes or reading core dumps cannot get any
-     information.  We do it in this way to clear correct_words[]
-     inside the SHA256 implementation as well.  */
-  sha256_init_ctx (&ctx);
-  sha256_finish_ctx (&ctx, alt_result);
-  memset (temp_result, '\0', sizeof (temp_result));
-  memset (p_bytes, '\0', key_len);
-  memset (s_bytes, '\0', salt_len);
-  memset (&ctx, '\0', sizeof (ctx));
-  memset (&alt_ctx, '\0', sizeof (alt_ctx));
-  if (copied_key != NULL)
-    memset (copied_key, '\0', key_len);
-  if (copied_salt != NULL)
-    memset (copied_salt, '\0', salt_len);
-
-  return buffer;
+  *cp = '\0';
 }
 
 void
