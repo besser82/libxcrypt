e80e3e6665e5371911c7c7fb7ca958cd4f16dddd
Björn Esser <besser82@fedoraproject.org>
2018-08-25 16:50:16 +0200

    Implement crypt and gensalt for scrypt.

--- alg-yescrypt-opt.c
+++ alg-yescrypt-opt.c
@@ -30,7 +30,7 @@
 
 #include "crypt-port.h"
 
-#if INCLUDE_yescrypt
+#if INCLUDE_yescrypt || INCLUDE_scrypt
 
 #pragma GCC diagnostic ignored "-Wcast-align"
 #ifdef __clang__
@@ -1426,4 +1426,4 @@ int yescrypt_free_local(yescrypt_local_t *local)
 	return free_region(local);
 }
 
-#endif /* INCLUDE_yescrypt */
+#endif /* INCLUDE_yescrypt || INCLUDE_scrypt */
