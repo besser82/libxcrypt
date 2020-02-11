aa1b73a90614fd4f8fa76edcaaee4c4ff50b5f6c
Zack Weinberg <zackw@panix.com>
2020-02-10 19:48:34 -0500

    Don’t use formulae.brew.sh in .travis_after_success.sh either.
    I should have grepped the source tree for this construct instead of
    assuming there was only one use.

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,29 +1,30 @@
-#!/bin/bash
+#! /bin/sh
+
+if [ "$PERFORM_COVERITY_SCAN" = 1 ] || [ "$DISTCHECK" = 1 ]; then
+  exit 0
+fi
+
 set -e
-
-if [[ "$PERFORM_COVERITY_SCAN" == "1" ]]; then
-  exit 0
-fi
-
-if [[ "$DISTCHECK" == "1" ]]; then
-  exit 0
-fi
-
-export GCOV="gcov"
-
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  GCC_VER="$(curl -s https://formulae.brew.sh/api/formula/gcc.json | jq -r '.versions.stable' | cut -d. -f1)"
-  if [[ "$CC" == "gcc" ]] || [[ "$CC" == "gcc-$GCC_VER" ]]; then
-    export GCOV="gcov-$GCC_VER"
-  fi
-elif [[ "$TRAVIS_OS_NAME" == "linux" ]] && [[ "$CC" == "clang" ]]; then
-  export GCOV="$PWD/.clang_gcov_wrapper.sh"
-fi
-
 . build/venv/bin/activate
+
+GCOV=gcov
+
+if [ "$CC" = "clang" ]; then
+  GCOV="$PWD/scripts/clang-gcov-wrapper"
+
+elif [ "$TRAVIS_OS_NAME" = osx ]; then
+  GCC_VER="$( (brew list --versions gcc || echo gcc 0) |
+              sed 's/^gcc \([0-9]*\)\..*$/\1/' )"
+  if [ "$CC" = gcc ] || [ "$CC" = "gcc-$GCC_VER" ]; then
+    GCOV="gcov-$GCC_VER"
+  fi
+fi
+export GCOV
+
 set -x
 
 lcov --gcov-tool $GCOV --directory . --capture --output-file all_coverage.info
-lcov --gcov-tool $GCOV --remove all_coverage.info '/usr/*' '*test*' > coverage.info
+lcov --gcov-tool $GCOV --remove all_coverage.info \
+     '/usr/*' '*test*' > coverage.info
 rm all_coverage.info
 codecov -X gcov
