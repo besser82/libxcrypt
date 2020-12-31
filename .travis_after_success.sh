e5363a906a9ea8ec02fce39c29c960997fa93cf2
Zack Weinberg <zackw@panix.com>
2020-12-31 12:02:02 -0500

    CI: Log identities of build and coverage tools.
    In .travis_before_script.sh and .travis_after_success.sh, log the full
    path and the --version output of each basic tool that we require.
    
    The before script does the build tools, and the after script does the
    coverage tools.  (This is because only the after script knows exactly
    which version of gcov we’re going to run.)

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+set -e
 
 if [ "$PERFORM_COVERITY_SCAN" = 1 ] || [ "$DISTCHECK" = 1 ]; then
   exit 0
@@ -25,8 +26,22 @@ elif [ "$TRAVIS_OS_NAME" = linux ]; then
 fi
 export GCOV
 
-set -x
+# Log the identities and versions of the coverage tools.
+for tool in "$GCOV" lcov codecov
+do
+    # $tool might include mandatory command-line arguments.
+    # Interpret it the same way Make would.
+    set fnord $tool
+    shift
+    if command -V $1; then
+        echo + "$@" --version
+        "$@" --version
+    fi
+    echo
+done
+set fnord; shift  # clear $@
 
+set -x
 lcov --gcov-tool $GCOV --directory . --capture --output-file all_coverage.info
 lcov --gcov-tool $GCOV --remove all_coverage.info \
      '/usr/*' '*test*' > coverage.info
--- .travis_before_script.sh
+++ .travis_before_script.sh
@@ -1,4 +1,28 @@
-#!/bin/bash
+#! /bin/sh
 set -e
 
-./autogen.sh
+# Log the identities and versions of the build tools.
+for tool in \
+    "${CC-cc}" \
+    "${AUTOCONF-autoconf}" \
+    "${AUTOMAKE-automake}" \
+    "${LIBTOOLIZE-libtoolize}" \
+    "${PKG_CONFIG-pkg-config}" \
+    "${PERL-perl}" \
+    "${PYTHON-python3}"
+do
+    # $tool might include mandatory command-line arguments.
+    # Interpret it the same way Make would.
+    set fnord $tool
+    shift
+    if command -V $1; then
+        echo + "$@" --version
+        "$@" --version
+    fi
+    echo
+done
+set fnord; shift  # clear $@
+
+# Prepare the configure scripts.
+set -x
+. ./autogen.sh
