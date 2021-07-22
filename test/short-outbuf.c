d9aa8295a1756b37b6d9f99539fc58f9f80545f3
Björn Esser <besser82@fedoraproject.org>
2021-07-22 20:40:09 +0200

    test/short-outbuf.c: Fix 'SIZEOF_MISMATCH' found by Covscan.
    CWE-569: Wrong sizeof argument (SIZEOF_MISMATCH)
    
    suspicious_sizeof: Passing argument '8UL /* sizeof (char *) */ * *j'
    to function malloc and then casting the return value to 'char *' is
    suspicious.

--- test/short-outbuf.c
+++ test/short-outbuf.c
@@ -38,21 +38,17 @@ static const struct testcase testcases[] =
 int
 main (void)
 {
-  bool ok       = true;
-  char **outbuf = malloc (sizeof (char*));
+  bool ok = true;
   char result[5];
 
   for (size_t i = 0; i < ARRAY_SIZE (testcases); i++)
     {
-      size_t *j = malloc (sizeof (size_t));
+      size_t j = i + 1;
+      char *outbuf = malloc (sizeof (char) * j);
 
-      *j = i + 1;
+      crypt_rn ("@@", "@@", outbuf, (int) j);
 
-      *outbuf = malloc (sizeof (char*) * *j);
-
-      crypt_rn ("@@", "@@", *outbuf, (int) *j);
-
-      if (!strncmp (testcases[i].exp_rn, *outbuf, *j))
+      if (!strncmp (testcases[i].exp_rn, outbuf, j))
         {
           strcpy (result, "PASS");
         }
@@ -63,11 +59,11 @@ main (void)
         }
 
       printf ("Test %zu.0: %s, expected: \"%-2s\", got: \"%-2s\"\n",
-              i + 1, result, testcases[i].exp_rn, *outbuf);
+              j, result, testcases[i].exp_rn, outbuf);
 
-      crypt_ra ("@@", "@@", (void **) outbuf, (int *) j);
+      crypt_ra ("@@", "@@", (void **) &outbuf, (int *) &j);
 
-      if (!strncmp (testcases[i].exp_ra, *outbuf, strlen(*outbuf)))
+      if (!strncmp (testcases[i].exp_ra, outbuf, strlen(outbuf)))
         {
           strcpy (result, "PASS");
         }
@@ -78,13 +74,10 @@ main (void)
         }
 
       printf ("Test %zu.1: %s, expected: \"%-2s\", got: \"%-2s\"\n",
-              i + 1, result, testcases[i].exp_ra, *outbuf);
+              i + 1, result, testcases[i].exp_ra, outbuf);
 
-      free (j);
-      free (*outbuf);
+      free (outbuf);
     }
-
-  free (outbuf);
 
   return ok ? 0 : 1;
 }
