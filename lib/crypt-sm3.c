1b3bbed828ccecbf99271755b7066d25e7d50f3d
Ondřej Hošek <ondra.hosek@gmail.com>
2026-02-11 13:04:31 +0100

    lib/crypt-sha*, lib/crypt-sm3: fix misleading comment
    A copied comment misrepresents the calculation step of the S byte
    sequence in the crypt algorithms based on SHA2 and sm3. Replace it with
    an actual description of what is happening.

--- lib/crypt-sm3.c
+++ lib/crypt-sm3.c
@@ -213,7 +213,8 @@ crypt_sm3crypt_rn (const char *phrase, size_t phr_size,
   /* Start computation of S byte sequence.  */
   sm3_init (ctx);
 
-  /* For every character in the password add the entire password.  */
+  /* Add the salt n times where n is the value of the first byte of the
+     intermediate result plus 16.  */
   for (cnt = 0; cnt < (size_t) 16 + (size_t) result[0]; ++cnt)
     sm3_update (ctx, salt, salt_size);
 
